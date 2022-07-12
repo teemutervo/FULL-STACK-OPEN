d0283b95266116628febab7efb337aa0e60801c3
unknown <teemu.tervo@hotmail.com>
2022-07-12 19:01:04 +0300

    1st commit

---
+++ kurssitiedot/src/App.js
@@ -0,0 +1,64 @@
+const Header = (props) => {
+  return (
+  <div>
+    <h1>{props.course}
+    </h1>
+  </div>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <>
+      <p>{props.partValue} {props.exercises}</p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part partValue={props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part partValue={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part partValue={props.parts[2].name} exercises={props.parts[2].exercises}/>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+  <>
+    <p>Number of exercises {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}</p>
+  </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+  <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+
+    </div>
+  )
+}
+
+export default App
